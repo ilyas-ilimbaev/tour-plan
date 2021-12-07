db4c698f5d9863ad492ec828e0477fc7f63aeb13
Ильяс Илимбаев <ilyas.ilimbaev@yandex.ru>
2021-12-08 02:52:22 +0500

    Adding a geo-map and reservation block

--- js/main.js
+++ js/main.js
@@ -22,3 +22,21 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+$(window).scroll(function(){
+  $('.newsletter').bgscroll({
+      direction: 'bottom', // направление bottom или top
+      bgpositionx: 50, // x позиция фонового изображения, от 0 до 100, размерность в %, 50 - означает по центру
+      debug: false, // Режим отладки
+      min:0, // минимальное положение (в %) на которое может смещаться фон
+      max:100 // максимальное положение (в %) на которое может смещаться фон
+  });
+})
+var myMap;
+ymaps.ready(function(){
+    // Указывается идентификатор HTML-элемента.
+    var moscow_map = new ymaps.Map("map", {
+        center: [37.796059, -122.395892],
+        zoom: 16
+    });
+});
